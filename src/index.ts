98df7bfff36ee7be4f072ecdc88753d42abd441d
Gabriel <gabrielbuxo1@gmail.com>
2023-10-06 14:31:48 +0200

    added login page and tailwind

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,19 @@ dotenv.config()
 const PORT = process.env.PORT || 3000
 const app = express()
 
-app.use(express.static(path.resolve(__dirname, "../static")))
+app.use(express.static(path.join(__dirname, '../static')))
 app.set('views', path.join(__dirname, '../views'))
 app.set("view engine", "pug")
-app.get("/", (req, res) => {
-    res.render("index", {title: "Todo App", message: "hello world!"})
+
+app.get("/", (req, res, next) => {
+    req.url = "/login"
+    next()
+})
+app.get("/login", (req, res) => {
+    res.render("login", {title: "Todo App login"})
+})
+app.get("/register", (req, res) => {
+    res.render("login", {title: "Todo App register"})
 })
 
 app.listen(PORT, () => {
