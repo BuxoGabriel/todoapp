49584cf1a5bf5e91646b8df359f4b8f514f30386
Gabriel <gabrielbuxo1@gmail.com>
2023-10-06 03:01:49 +0200

    added prisma

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
+import path from "path"
 import express from "express"
 import dotenv from "dotenv"
 dotenv.config()
 
 const PORT = process.env.PORT || 3000
 const app = express()
+
+app.use(express.static(path.resolve(__dirname, "../static")))
+app.set('views', path.join(__dirname, '../views'))
+app.set("view engine", "pug")
+app.get("/", (req, res) => {
+    res.render("index", {title: "Todo App", message: "hello world!"})
+})
 
 app.listen(PORT, () => {
     console.log("listening on port " + PORT)
